8df743c9e7d3d4e2bf3681d6fdf91e23a8905a81
seoY0206 <lsy19220@gmail.com>
2025-07-15 15:12:03 +0900

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -12,13 +12,16 @@ import time
 
 from selenium.webdriver.chrome.options import Options
 
+from selenium.webdriver.chrome.service import Service
+service = Service(executable_path="/usr/bin/chromedriver")
+
 options = Options()
 options.add_argument("--headless") # 창 없음
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 url_pokemon = "https://pokemonkorea.co.kr/pokedex"
-driver = wb.Chrome(options=options)
+driver = wb.Chrome(service=service, options=options)
 driver.maximize_window()
 driver.get(url_pokemon)
 img = driver.find_element(By.CSS_SELECTOR, "img.img-fluid")
